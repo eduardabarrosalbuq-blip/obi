ff6c3d1b91db6667153ee08b8fe203b22c159350
eduardabarrosalbuq-blip <eduardabarrosalbuq@gmail.com>
2026-02-22 17:28:07 +0000

    OBI

---
+++ calculadora.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int a, b, soma;
+
+    cout << "Qual é o primeiro número?";
+    cin >> a;
+
+    cout << "Qual é o segundo número?";
+    cin >> b;
+
+    soma = a + b;
+
+    cout << "A soma é " << soma << "\n";
+
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    cout << "Olá, mundo" << "\n";
+
+    return 0;
+}
