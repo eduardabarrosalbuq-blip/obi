ca726d8ac18b14d678484f4c5b798faf134ad072
eduardabarrosalbuq-blip <eduardabarrosalbuq@gmail.com>
2026-04-18 12:57:25 +0000

    Aula 7

---
+++ problemas 2020/emoticons.cpp/emoticon.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(void){
+    string linha;
+int D= 0, C =0
+
+    getline (cin, linha);
+
+    for(basic string<char> i = linha.begin(); (i < linha.end()-2); i++){
+if( linh[i] == ':' && linha[i+1] == '-' && linha[i+2] == ')'){
+D++
+}
+if( linh[i] == ':' && linha[i+1] == '-' && linha[i+2] == '('){
+C++
+ }
+ if (D > c){
+    cout << "divertido\n";
+ }else if (D < C);
+
+    cout << linha << "\n";
+
+return 0;
+}
---
+++ problemas 2020/emoticons.cpp/fissuraperigosa.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int msin(void){
+
+    
+return 0;
+}
